060c535237a7a548af2756d83e66f2b0a0aefb90
Leo <ujfox13@gmail.com>
2021-09-23 19:46:51 +0300

    Roman Numerals helper

--- 4K/Roman Numerals Helper/script.js
+++ 4K/Roman Numerals Helper/script.js
@@ -1,20 +1,17 @@
 
 class RomanNumerals {
-    constructor(num) {
-        this.num = num;
-    }
 
-    toRoman() {
-    
+    static toRoman(num) {
+
         let romanNum = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
     
-        let str = String(this.num);
+        let str = String(num);
         let pos = str.length;
         let result = '';
         
         for (let key of str) { //итерируем каждый разряд
     
-            let romL = romanNum.length + 1 - pos * 2  ; // -4 // -6 
+            let romL = romanNum.length + 1 - pos * 2  ; 
             if (key < 4) {
                 for ( let i = 0; i < key; i++) {
                     result += `${romanNum[romL]}`; //I
@@ -31,7 +28,7 @@ class RomanNumerals {
     
             switch (key) {
                 case '4':
-                    result += `${romanNum[romL]}${romanNum[romL-1]}` // 'IV';
+                    result += `${romanNum[romL]}${romanNum[romL-1]}`; // 'IV';
                     break;
                 case '5':
                     result += `${romanNum[romL-1]}`;
@@ -42,81 +39,41 @@ class RomanNumerals {
                 default:
                     break;
             }
-            pos--
-            //console.log(result);
+            pos--;
         }
         
-        return result
+        return result;
     }
     
-    fromRoman(num) {
+    static fromRoman(num) {
+        let numeric = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+
+        let arr = num.split('');
+        let numArr = [];
+        
+        arr.forEach( elem => numArr.push(numeric[elem]) );
+
+        numArr.map( (elem, i) => {
+            if (numArr[i + 1]) {
+                if (numArr[i] < numArr[i + 1]){
+                    numArr[i + 1] = numArr[i + 1] - numArr[i];
+                    numArr[i] = 0;
+                }
+            }
+        });
+
+        return numArr.reduce( (a, b) => a + b );
 
     }
 }
 
-let numeral = new RomanNumerals(1456)
-console.log(numeral.toRoman()); ; // should return 'M'
-
-RomanNumerals.toRoman(1000);
-//RomanNumerals.fromRoman('M'); // should return 1000
-
-/* let foo = 3999;
-console.log(String(foo)[0]);
-
-
-let arabicNum = [1000, 500, 100, 50, 10, 5, 1, ];
-
-
-console.log(toRoman(3999)); 
-console.log(toRoman(2999)); 
-console.log(toRoman(1999)); 
-
-
-//3999   MMMCMXCIX
-
-let romeNum = [1000, 1000, 1000, 100, 1000, 10, 100, 1, 10];
-let arr = [];
-let result
-romeNum.forEach((elem, i) => {
-    
-    if(romeNum[i] < romeNum[i + 1]) {
-        arr.push()
-    } else if (){
-
-    } else {
-        arr.push(elem);
-    }
-})
-
-
- */
-/*
-
-I 	1
-IV 	4
-V 	5
-X 	10
-L 	50
-C 	100
-D 	500
-M 	1000
-
-
-Создайте класс RomanNumerals, который может преобразовывать римские числа 
-в целочисленные значения и обратно. 
-Он должен соответствовать API, продемонстрированному в примерах ниже. 
-Для каждого вспомогательного метода будет проверено несколько значений 
-римских цифр.
-
-Современные римские цифры записываются, выражая каждую цифру отдельно, 
-начиная с самой левой цифры и пропуская любую цифру с нулевым значением. 
-Римскими цифрами отображается 1990 год: 1000 = M, 900 = CM, 90 = XC; 
-в результате получается MCMXC. 
-2008 записывается как 2000 = MM, 8 = VIII; или MMVIII. 
-1666 использует каждый римский символ в порядке убывания: MDCLXVI.
-
-Диапазон ввода: 1 <= n <4000
-
-В этом ката 4 должны быть представлены как IV, 
-а НЕ как IIII («часовая четверка»).
-*/
+console.log(RomanNumerals.toRoman(1946)); 
+console.log(RomanNumerals.fromRoman('MCMXLVI')); // should return 1946
